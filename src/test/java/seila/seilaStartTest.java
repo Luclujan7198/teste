96e4a91d346bff5f1cd2858fe81f1dc3b67157db
luclujan7198 <Kilzone 10>
2019-03-16 20:47:01 -0300

    Primeiro commit

---
+++ src/test/java/seila/seilaStartTest.java
@@ -0,0 +1,14 @@
+package seila;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class seilaStartTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
