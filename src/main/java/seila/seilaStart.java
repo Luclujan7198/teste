49a93ecbe934b4cfefd85cd6846d5675fda67aa9
luclujan7198 <Kilzone 10>
2019-03-29 17:22:08 -0300

    nova versao

--- src/main/java/seila/seilaStart.java
+++ src/main/java/seila/seilaStart.java
@@ -5,6 +5,7 @@ public class seilaStart {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ola Mundo");
+		System.out.println("teste nova versao");
 	}
 
 }
